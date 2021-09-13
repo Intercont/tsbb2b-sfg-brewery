cf2111799350f62ecabf91b20af49f2ecd772a62
Igor A Fraga <igoravancinifraga@gmail.com>
2021-09-13 17:53:03 -0300

    183 - Using JsonPath with Lists - Spring MVC Rest Controller Tests

--- src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -2,21 +2,30 @@ package guru.springframework.brewery.web.controllers;
 
 import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerPagedList;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -76,4 +85,62 @@ class BeerControllerTest {
                 .andExpect(jsonPath("$.upc", is(validBeer.getUpc())))
                 .andExpect(jsonPath("$.beerStyle", is(BeerStyleEnum.PALE_ALE.toString())));
     }
+
+    @DisplayName("Test List Operations - ")
+    @Nested
+    public class TestListOperations {
+
+        @Captor
+        ArgumentCaptor<String> beerNameCaptor;
+
+        @Captor
+        ArgumentCaptor<BeerStyleEnum> beerStyleEnumCaptor;
+
+        @Captor
+        ArgumentCaptor<PageRequest> pageRequestCaptor;
+
+        BeerPagedList beerPagedList;
+
+        UUID cachacaId;
+
+        @BeforeEach
+        void setUp() {
+            List<BeerDto> beers = new ArrayList<>();
+            beers.add(validBeer);
+
+            cachacaId = UUID.randomUUID();
+
+            beers.add(BeerDto.builder()
+                    .id(cachacaId)
+                    .version(1)
+                    .beerName("Cachaça do Igor")
+                    .upc(12313548984L)
+                    .beerStyle(BeerStyleEnum.SAISON)
+                    .price(new BigDecimal("1.99"))
+                    .quantityOnHand(456)
+                    .createdDate(OffsetDateTime.now())
+                    .lastModifiedDate(OffsetDateTime.now())
+                    .build());
+
+            beerPagedList = new BeerPagedList(beers, PageRequest.of(1, 1), 2L);
+
+            given(beerService.listBeers(beerNameCaptor.capture(), beerStyleEnumCaptor.capture(), pageRequestCaptor.capture()))
+                    .willReturn(beerPagedList);
+        }
+
+        @DisplayName("Test list of beers - no parameters")
+        @Test
+        void testListBeers() throws Exception {
+
+            mockMvc.perform(get("/api/v1/beer")
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                    .andExpect(jsonPath("$.content", hasSize(2)))
+                    .andExpect(jsonPath("$.content[0].id", is(validBeer.getId().toString())))
+                    .andExpect(jsonPath("$.content[1].id", is(cachacaId.toString())));
+
+        }
+    }
+
 }
